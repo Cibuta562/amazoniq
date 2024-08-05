ee208935d0d77189f3daab038d938081d24e8bfd
robertandreiboboc <andreiboboccibo@gmail.com>
2024-08-05 20:52:21 +0300

    update dropdown preview v2

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -2,9 +2,65 @@ import React from 'react';
 import './ProductGrid.css';
 import Product from "./ProductContainer";
 import coldBrewTonic from "../assets/cold-brew-water.png"
+import espresso from "../assets/img_1.png"
+import dubluEspresso from "../assets/img_2.png"
+import longBlack from "../assets/img_3.png"
+import v60single from "../assets/img_4.png"
 
 const ProductGrid = () => {
     const products = [
+        {
+            name: 'SINGLE ESPRESSO',
+            price: '20 ml',
+            description: '8g cafea Mabo, apă fierbinte',
+            image: espresso
+        },
+        {
+            name: 'DOUBLE ESPRESSO',
+            price: '40 ml',
+            description: '16g cafea Mabo, apă fierbinte',
+            image: dubluEspresso
+        },
+        {
+            name: 'LONG BLACK',
+            price: '80 ml',
+            description: 'Espresso dublu 40 ml + 40 ml apă fierbinte',
+            image: longBlack
+        },
+        {
+            name: 'V60 SINGLE ORIGIN',
+            price: '350 ml',
+            description: 'Cafea la filtru SINGLE ORIGIN',
+            image: v60single
+        },
+        {
+            name: 'DOUBLE ESPRESSO DECAF',
+            price: '300 ml',
+            description: '80 ml cold brew + \n' +
+                '200 ml apă tonică Franklin + gheata + garnish + cox mult',
+            image: coldBrewTonic
+        },
+        {
+            name: 'DOUBLE ESPRESSO DECAF',
+            price: '300 ml',
+            description: '80 ml cold brew + \n' +
+                '200 ml apă tonică Franklin + gheata + garnish + cox mult',
+            image: coldBrewTonic
+        },
+        {
+            name: 'DOUBLE ESPRESSO DECAF',
+            price: '300 ml',
+            description: '80 ml cold brew + \n' +
+                '200 ml apă tonică Franklin + gheata + garnish + cox mult',
+            image: coldBrewTonic
+        },
+        {
+            name: 'DOUBLE ESPRESSO DECAF',
+            price: '300 ml',
+            description: '80 ml cold brew + \n' +
+                '200 ml apă tonică Franklin + gheata + garnish + cox mult',
+            image: coldBrewTonic
+        },
         {
             name: 'DOUBLE ESPRESSO DECAF',
             price: '300 ml',
