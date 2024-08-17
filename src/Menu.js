833656a471d47db14a60d414afd20a612626d901
robertandreiboboc <andreiboboccibo@gmail.com>
2024-08-17 14:36:32 +0300

    google perf update 1

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Menu.css';
-import bg from "./assets/Amazoniq Inline Coal 3.svg";
+import bg from "./assets/AMAZONIQ INLINE COAL CLEAR.svg";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
--- src/pages/meniu.js
+++ src/pages/meniu.js
@@ -1,14 +1,17 @@
 import ProductGrid from "../components/ProductGrid";
+import ScrollAnimation from 'react-animate-on-scroll';
 import "./meniu.css"
 
 function Meniu() {
 return(
+    <ScrollAnimation animateIn="fadeIn">
     <div className="meniu-bg">
         <ProductGrid/>
         <br/>
         <br/>
         <br/>
     </div>
+    </ScrollAnimation>
 )
 }
 
