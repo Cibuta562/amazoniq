a201cf34455a2517fce1dcecf7cd78f3dbd94254
robertandreiboboc <andreiboboccibo@gmail.com>
2024-09-25 11:15:41 +0300

    Update Assets and Footer Pages

--- src/pages/prelucrareaDatelor.js
+++ src/pages/prelucrareaDatelor.js
@@ -4,10 +4,34 @@ import Footer from "./footer";
 
 
 function PrelucrareaDatelor() {
+
+    const fetchPlaceDetails = async () => {
+        const placeId = 'ChIJHzs_1HgBskARCilzkRzmOvM'; // Your Place ID
+        const apiKey = 'AIzaSyDsNHChTA7IfJQiecyPMD_JvGco7uNg8YY'; // Replace with your API key
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${apiKey}`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Place Details:', data);
+                // Use data.result.reviews to access reviews
+            } else {
+                console.error('Error fetching data:', data.error_message);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+// Call the function to fetch place details
+
     return (<div className="prelucrareaDatelor-cont">
         <Menu/>
         <div className="prelucrareaDatelor-container">
             <p className="date-p">Prelucrarea Datelor</p>
+            <button onClick={fetchPlaceDetails}>Get Data</button>
         </div>
         <Footer/>
     </div>)
